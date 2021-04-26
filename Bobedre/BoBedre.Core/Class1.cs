3a1efd035955899ad8d2c80eb8708e4a41d312f8
Nikcio <nikolajgive@gmail.com>
2021-04-26 08:24:28 +0200

    Project setup

---
+++ Bobedre/BoBedre.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BoBedre.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Bobedre/BoBedre.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BoBedre.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
