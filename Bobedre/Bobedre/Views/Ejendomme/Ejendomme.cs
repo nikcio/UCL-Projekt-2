5ade7ff46a1531e9d1616fc5b0261d6000b31acd
Nikcio <nikolajgive@gmail.com>
2021-04-28 11:55:54 +0200

    Update Ejendomme.cs

--- Bobedre/Bobedre/Views/Ejendomme/Ejendomme.cs
+++ Bobedre/Bobedre/Views/Ejendomme/Ejendomme.cs
@@ -19,65 +19,65 @@ namespace Bobedre.Views.Ejendomme
             InitializeComponent();
         }
 
-        //private void OpretBoligKnap_Click(object sender, EventArgs e)
-        //{
-           
-        //    String sSQL = "INSERT INTO Ejendome VALUES (@Bolignr, @Adresse, @Pris, @Boligareal, @Grundareal, @have, @værelser, @Etager, @Type, @Byggeår, @Renoveret, @Køkken, @Badeværelse, @Andet, @Ombygningsår, @Detaljier, @RenoveringsId)";
-        //    SqlCommand cmd = new SqlCommand(sSQL);
-            
-        //    cmd.Parameters.AddWithValue("@Bolignr", int.Parse(BolignrLabel.Text));
-        //    cmd.Parameters.AddWithValue("@Adresse", (AdresseBolig.Text));
-        //    cmd.Parameters.AddWithValue("@Pris", int.Parse(PrisTextBox.Text));
-        //    cmd.Parameters.AddWithValue("@Boligareal", int.Parse(BoligArealTextBox.Text));
-        //    cmd.Parameters.AddWithValue("@Grundareal", int.Parse(GrundArealBoligTextBox.Text));
-        //    cmd.Parameters.AddWithValue("@Have", (HaveBoligTextBox.Text));
-        //    cmd.Parameters.AddWithValue("@Værelser", int.Parse(VæreslerBoligTextBox.Text));
-        //    cmd.Parameters.AddWithValue("@Typebolig", (TypeBoligTextBox.Text));
-        //    cmd.Parameters.AddWithValue("@Byggeår", int.Parse(ByggeårBoligTextBox.Text));
-        //    cmd.Parameters.AddWithValue("@Bolignr", int.Parse(RenoveretBoligCheckBox.Text));
+        private void OpretBoligKnap_Click(object sender, EventArgs e)
+        {
 
-        //    if (Regex.IsMatch((BolignrLabel.Text) + (AdresseBolig.Text) + (PrisTextBox.Text) + (BoligArealTextBox.Text)
-        //        + (GrundArealBoligTextBox.Text) + (HaveBoligTextBox.Text) + (VæreslerBoligTextBox.Text) + (TypeBoligTextBox.Text)
-        //        + (ByggeårBoligTextBox.Text) + (ByggeårBoligTextBox.Text) + (RenoveretBoligCheckBox), @"^[a-z A-Z]+$"))
-        //    {
+            String sSQL = "INSERT INTO Ejendome VALUES (@Bolignr, @Adresse, @Pris, @Boligareal, @Grundareal, @have, @værelser, @Etager, @Type, @Byggeår, @Renoveret, @Køkken, @Badeværelse, @Andet, @Ombygningsår, @Detaljier, @RenoveringsId)";
+            SqlCommand cmd = new SqlCommand(sSQL);
 
-        //    }
+            cmd.Parameters.AddWithValue("@Bolignr", int.Parse(BolignrLabel.Text));
+            cmd.Parameters.AddWithValue("@Adresse", (AdresseBolig.Text));
+            cmd.Parameters.AddWithValue("@Pris", int.Parse(PrisTextBox.Text));
+            cmd.Parameters.AddWithValue("@Boligareal", int.Parse(BoligArealTextBox.Text));
+            cmd.Parameters.AddWithValue("@Grundareal", int.Parse(GrundArealBoligTextBox.Text));
+            cmd.Parameters.AddWithValue("@Have", (HaveBoligTextBox.Text));
+            cmd.Parameters.AddWithValue("@Værelser", int.Parse(VæreslerBoligTextBox.Text));
+            cmd.Parameters.AddWithValue("@Typebolig", (TypeBoligTextBox.Text));
+            cmd.Parameters.AddWithValue("@Byggeår", int.Parse(ByggeårBoligTextBox.Text));
+            cmd.Parameters.AddWithValue("@Bolignr", int.Parse(RenoveretBoligCheckBox.Text));
+
+            if (Regex.IsMatch((BolignrLabel.Text) + (AdresseBolig.Text) + (PrisTextBox.Text) + (BoligArealTextBox.Text)
+                + (GrundArealBoligTextBox.Text) + (HaveBoligTextBox.Text) + (VæreslerBoligTextBox.Text) + (TypeBoligTextBox.Text)
+                + (ByggeårBoligTextBox.Text) + (ByggeårBoligTextBox.Text) + (RenoveretBoligCheckBox), @"^[a-z A-Z]+$"))
+            {
+
+            }
 
 
 
-        //}
+        }
 
-        //private void SletButtonBolig_Click(object sender, EventArgs e)
-        //{
-        //    string sSQL = "DELETE FROM Ejendome where Bolignr =@Bolignr";
+        private void SletButtonBolig_Click(object sender, EventArgs e)
+        {
+            string sSQL = "DELETE FROM Ejendome where Bolignr =@Bolignr";
 
-        //    SqlCommand cmd = new SqlCommand(sSQL);
-        //    cmd.Parameters.AddWithValue("@Bolingnr", int.Parse(BolignrLabel.Text));
+            SqlCommand cmd = new SqlCommand(sSQL);
+            cmd.Parameters.AddWithValue("@Bolingnr", int.Parse(BolignrLabel.Text));
 
-        //    MessageBox.Show("Boligen er nu slettet");
-        //    BolignrLabel.ResetText();
+            MessageBox.Show("Boligen er nu slettet");
+            BolignrLabel.ResetText();
 
-        //    cmd.ExecuteNonQuery();
-            
+            cmd.ExecuteNonQuery();
 
-        //}
 
-        //private void OpdaterBoligKnap_Click(object sender, EventArgs e)
-        //{
-            
-        //    string sSQL = "UPDATE Ejendome set (Bolingnr=@Bolignr, Adresse=@Adresse, Pris=@Pris, Boligareal=@Boligareal, Grundareal=@Grundareal, Have=@Have, Værelser=@værelser, Etager=@Etager, Type=@Type, Byggeår=@Byggeår, Renoveret=@Renoveret, Køkken=@Køkken, Badeværelse=@Badeværelse, Andet=@Andet, Ombygningsår=@Ombygningsår, Detalijer=@Detaljier, RenoveringsId=@RenoveringsId)";
-        //    SqlCommand cmd = new SqlCommand(sSQL);
+        }
 
-        //    cmd.Parameters.AddWithValue("@Bolignr", int.Parse(BolignrLabel.Text));
-        //    cmd.Parameters.AddWithValue("@Adresse", (AdresseBolig.Text));
-        //    cmd.Parameters.AddWithValue("@Pris", int.Parse(PrisTextBox.Text));
-        //    cmd.Parameters.AddWithValue("@Boligareal", int.Parse(BoligArealTextBox.Text));
-        //    cmd.Parameters.AddWithValue("@Grundareal", int.Parse(GrundArealBoligTextBox.Text));
-        //    cmd.Parameters.AddWithValue("@Have", (HaveBoligTextBox.Text));
-        //    cmd.Parameters.AddWithValue("@Værelser", (VæreslerBoligTextBox.Text));
-        //    cmd.Parameters.AddWithValue("@Typebolig", (TypeBoligTextBox.Text));
-        //    cmd.Parameters.AddWithValue("@Byggeår", int.Parse(ByggeårBoligTextBox.Text));
-        //    cmd.Parameters.AddWithValue("@Bolignr", int.Parse(RenoveretBoligCheckBox.Text));
-        //}
+        private void OpdaterBoligKnap_Click(object sender, EventArgs e)
+        {
+
+            string sSQL = "UPDATE Ejendome set (Bolingnr=@Bolignr, Adresse=@Adresse, Pris=@Pris, Boligareal=@Boligareal, Grundareal=@Grundareal, Have=@Have, Værelser=@værelser, Etager=@Etager, Type=@Type, Byggeår=@Byggeår, Renoveret=@Renoveret, Køkken=@Køkken, Badeværelse=@Badeværelse, Andet=@Andet, Ombygningsår=@Ombygningsår, Detalijer=@Detaljier, RenoveringsId=@RenoveringsId)";
+            SqlCommand cmd = new SqlCommand(sSQL);
+
+            cmd.Parameters.AddWithValue("@Bolignr", int.Parse(BolignrLabel.Text));
+            cmd.Parameters.AddWithValue("@Adresse", (AdresseBolig.Text));
+            cmd.Parameters.AddWithValue("@Pris", int.Parse(PrisTextBox.Text));
+            cmd.Parameters.AddWithValue("@Boligareal", int.Parse(BoligArealTextBox.Text));
+            cmd.Parameters.AddWithValue("@Grundareal", int.Parse(GrundArealBoligTextBox.Text));
+            cmd.Parameters.AddWithValue("@Have", (HaveBoligTextBox.Text));
+            cmd.Parameters.AddWithValue("@Værelser", (VæreslerBoligTextBox.Text));
+            cmd.Parameters.AddWithValue("@Typebolig", (TypeBoligTextBox.Text));
+            cmd.Parameters.AddWithValue("@Byggeår", int.Parse(ByggeårBoligTextBox.Text));
+            cmd.Parameters.AddWithValue("@Bolignr", int.Parse(RenoveretBoligCheckBox.Text));
+        }
     }
 }
