4c2a644d0bde216e6f07d3e99ea49b26d06fc96f
Wololo123 <73693446+Wololo123@users.noreply.github.com>
2021-04-29 10:45:49 +0200

    Create RegexCheck.cs

---
+++ Bobedre/BoBedre.Core/TextChecking/RegexCheck.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BoBedre.Core.TextChecking
+{
+    public static class RegexCheck
+    {
+        private static readonly string BasicTextRegex = @"^[a-z A-ZåæøÅÆØ]+$";
+        private static readonly string EmailRegex = "^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"; // Taken from: https://emailregex.com/
+        private static readonly string BaiscTalRegex = @"^[0-9]+$";
+
+        public static bool TextCheck(string textToCheck)
+        {
+            return Regex.IsMatch(textToCheck, BasicTextRegex);
+        }
+
+        public static bool EmailCheck(string textToCheck)
+        {
+            return Regex.IsMatch(textToCheck, EmailRegex);
+        }
+
+        public static bool TalCheck(string textToCheck)
+        {
+            return Regex.IsMatch(textToCheck, BaiscTalRegex);
+        }
+    }
+}
