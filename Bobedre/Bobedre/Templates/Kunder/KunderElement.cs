1027e6b64e40128412def94da960d1ae5b2af45b
Nikcio <nikolajgive@gmail.com>
2021-05-03 09:47:44 +0200

    Templates

---
+++ Bobedre/Bobedre/Templates/Kunder/KunderElement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bobedre.Templates.Kunder
+{
+    public partial class KunderElement : Form
+    {
+        public KunderElement()
+        {
+            InitializeComponent();
+        }
+    }
+}
