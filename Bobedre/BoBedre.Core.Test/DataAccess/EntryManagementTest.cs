0c7a4c156263872a4ab17805150322d98ee365db
andersen965 <74706368+andersen965@users.noreply.github.com>
2021-05-04 09:15:28 +0200

    Update EntryManagementTest.cs

--- Bobedre/BoBedre.Core.Test/DataAccess/EntryManagementTest.cs
+++ Bobedre/BoBedre.Core.Test/DataAccess/EntryManagementTest.cs
@@ -12,11 +12,33 @@ namespace BoBedre.Core.Test.DataAccess
     [TestClass]
     public class EntryManagementTest
     {
+        #region Ejendomægler
         [TestMethod]
-        public async Task CreateDeleteEjendomsmæglerTest()
+        public async Task CreateUpdateDeleteEjendomsmæglerTest()
         {
-            
-        }
+            //Create
+            string afdeling = "Afdeling";
+            string mæglerFirma = "Mæglerfirma";
+            string navn = "Navn";
+            string email = "Email";
+
+            var medarbejderNr = await EntryManagement.CreateEjendomsmægler(afdeling, mæglerFirma, navn, email);
+            var Ejendomsmægler = await Fetch.GetEjendomsmæglerByMedarbjederNr(medarbejderNr);
+
+            Assert.AreEqual(afdeling, Ejendomsmægler.Afdeling, "the value is not equal to the expected");
+            Assert.AreEqual(mæglerFirma, Ejendomsmægler.Mæglerfirma, "the value is not equal to the expected");
+            Assert.AreEqual(navn, Ejendomsmægler.Navn, "the value is not equal to the expected");
+            Assert.AreEqual(email, Ejendomsmægler.Email, "the value is not equal to the expected");
+
+
+            //delete
+            await EntryManagement.DeleteEjendomsmægler(medarbejderNr);
+            Assert.IsNull(await Fetch.GetEjendomsmæglerByMedarbjederNr(medarbejderNr));
+           
+        }    
+ 
+        #endregion
+
 
     }
 }
